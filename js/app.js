fc61d18969b698c8ab587302f7e9c8e8564507ea
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2024-02-07 21:39:45 +0530

    third commit

--- js/app.js
+++ js/app.js
@@ -9,8 +9,8 @@ let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 2000;
-let timeAutoNext = 4000;
+let timeRunning = 4000;
+let timeAutoNext = 6000;
 
 nextDom.onclick = function(){
     showSlider('next');    
